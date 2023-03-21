a8c29532efceb4675eef36858c3ed3a6439b0bb7
John Christopher Azcarraga <dyankristoper@anonymous-mini.local>
2023-03-21 10:26:23 +0800

    Feat: Updated routes for update and delete

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [TodoController::class, 'index'])->name('todos.index');
 // BRIEF: Write your routes here
 
-// POST Route - Create a new post
+// POST Route - Create a new todo
 Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
+
+// PUT Route - Update the current todo
+Route::put('/todos/{id}', [TodoController::class, 'update'])->name('todos.update');
+
+// DELETE Route - Delete an existing todo
+Route::delete('/todos', [TodoController::class, 'destroy'])->name('todos.destroy');
